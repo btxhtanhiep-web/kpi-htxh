1a1cdc137c11be1026f28d1cf9577cbddfa6424b
btxhtanhiep-web <btxhtanhiep@gmail.com>
2026-09-16 22:17:37 +0700

    Add files via upload

--- nhiem-vu/modules/periods/periods-view.js
+++ nhiem-vu/modules/periods/periods-view.js
@@ -1,2 +1,2 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260911.V1_23_1";
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260916.V1_24_7_HTXH_1";
 export async function renderPeriodsView(outlet){ await renderKpiWorkflow(outlet); }
